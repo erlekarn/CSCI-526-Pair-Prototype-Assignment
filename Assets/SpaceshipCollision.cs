e6be4aaa8d370568a9fa67090d2b828b0452ac3f
erlekarn <erlekar@usc.edu>
2023-09-21 20:12:21 -0700

    Added sprites and assets

--- Assets/SpaceshipCollision.cs
+++ Assets/SpaceshipCollision.cs
@@ -5,6 +5,7 @@ public class SpaceshipCollision: MonoBehaviour
     public Material greenMaterial; // Assign the green material in the Inspector.
     public int numberOfSpaceStationsCollided = 0;
     private bool gameEnd = false; // Track if the timer has started.
+    public Color whiteColor = Color.white; // Color for space stations after collision.
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,13 +13,13 @@ public class SpaceshipCollision: MonoBehaviour
         if (other.CompareTag("SpaceStation"))
         {
             // Change the material to green.
-            Renderer planetRenderer = other.GetComponent<Renderer>();
-            if (planetRenderer != null)
+            SpriteRenderer spaceStationRenderer = other.GetComponent<SpriteRenderer>();
+            if (spaceStationRenderer != null)
             {
                 numberOfSpaceStationsCollided += 1;
-                planetRenderer.material = greenMaterial;
+                spaceStationRenderer.color = Color.white;
 
-                if(numberOfSpaceStationsCollided == 6)
+                if (numberOfSpaceStationsCollided == 6)
                 {
                     gameEnd = true;
 
@@ -36,7 +37,7 @@ public class SpaceshipCollision: MonoBehaviour
             gameOverStyle.fontSize = 24; // Set the font size.
             gameOverStyle.fontStyle = FontStyle.Bold; // Make the text bold.
 
-            GUI.Label(new Rect(150, 105, 1000, 200), "YOU CURED ALL THE SPACE STATIONS. YOU WIN. GAME OVER", gameOverStyle);
+            GUI.Label(new Rect(80, 105, 1000, 200), "YOU CURED ALL THE SPACE STATIONS. YOU WIN. GAME OVER", gameOverStyle);
             Time.timeScale = 0f; // Pause the game.
         }
     }
